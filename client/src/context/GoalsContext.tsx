b1c15e0e6815a9f5d3a3fbed2e6a6b5bdf4c4eb6
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-08 16:14:40 -0500

    Create context to Goals

---
+++ client/src/context/GoalsContext.tsx
@@ -0,0 +1,51 @@
+import { createContext, useContext, useState, ReactNode } from "react";
+
+export type GoalCards = {
+    id: number,
+    content: string,
+    isComplete: boolean,
+    taskList: TaskList[];
+}
+
+type TaskList = {
+    id: number,
+    content: string,
+    isComplete: boolean,
+}
+
+interface GoalsContextType {
+    goalCards: GoalCards[];
+    setGoalCards: (goalCards: GoalCards[]) => void;
+}
+
+const defaultState: GoalsContextType = {
+    goalCards: [],
+    setGoalCards: (goalCards: GoalCards[]) => {}
+}
+
+const GoalsContext = createContext(defaultState);
+
+export const useGoals = () => {
+    const context = useContext(GoalsContext);
+    if (!context) {
+        throw new Error("useGoals must be used within a GoalsContextProvider");
+    } else {
+        return context;
+    }
+}
+
+type GoalsProviderProps = {
+    children: ReactNode,
+}
+
+function GoalsContextProvider({children} : GoalsProviderProps) {
+    const [goalCards, setGoalCards] = useState<GoalCards[]>([]);
+
+    return (
+        <GoalsContext.Provider value={{ goalCards, setGoalCards }}>
+            {children}
+        </GoalsContext.Provider>
+    )
+}
+
+export default GoalsContextProvider;
