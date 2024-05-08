184d6be169f097f6b767bd4f92bcf2bbe6fd66b7
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-08 13:10:32 -0500

    Create GoalCard component

---
+++ client/src/components/GoalCard.tsx
@@ -0,0 +1,28 @@
+import { GoalCards } from "../App";
+
+type Props = {
+    card: GoalCards;
+}
+
+function GoalCard({ card }: Props) {
+    return (
+        <div>
+            <div>{card.isComplete ? "Complete" : "No Complete"}</div>
+            <header>
+                <p>{card.content}</p>
+            </header>
+            <div>
+                <ul>
+                    {card.taskList.map((task, index) => (
+                        <li key={index}>
+                            <p>{task.content}</p>
+                            <span>{task.isComplete ? "✅" : "No Complete"}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default GoalCard;
