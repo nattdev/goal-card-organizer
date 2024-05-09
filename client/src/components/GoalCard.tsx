397f0ef96e70b297c4d8b612797b48abb52ccb8f
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-08 19:15:23 -0500

    Implement functionality to add task

--- client/src/components/GoalCard.tsx
+++ client/src/components/GoalCard.tsx
@@ -1,10 +1,35 @@
-import { GoalCards } from "../context/GoalsContext";
+import { GoalCards, TaskList, useGoals } from "../context/GoalsContext";
 
 type Props = {
     card: GoalCards;
 }
 
 function GoalCard({ card }: Props) {
+
+    const { goalCards, setGoalCards } = useGoals();
+
+    function handleAddTask() {
+        const updatedGoalCards = goalCards.map((goalCard) => {
+            if (goalCard.id === card.id) {
+                const newTask: TaskList = {
+                    id: goalCard.taskList.length + 1,
+                    content: "Nueva tarea",
+                    isComplete: false
+                };
+                return {
+                    ...goalCard,
+                    taskList: [...goalCard.taskList, newTask]
+                };
+            }
+            return goalCard;
+        });
+        setGoalCards(updatedGoalCards);
+    }
+
+    function handleOnBlur(e: React.FocusEvent<HTMLInputElement>) {
+        console.log(e.target.value);
+    }
+
     return (
         <div>
             <div>{card.isComplete ? "Complete" : "No Complete"}</div>
@@ -13,8 +38,10 @@ function GoalCard({ card }: Props) {
             </header>
             <div>
                 <ul>
+                    <p onClick={handleAddTask}>+ Añadir Tarea</p>
                     {card.taskList.map((task, index) => (
                         <li key={index}>
+                            <input type="text" defaultValue={task.content} onBlur={(e) => handleOnBlur(e)}></input>
                             <p>{task.content}</p>
                             <span>{task.isComplete ? "✅" : "No Complete"}</span>
                         </li>
