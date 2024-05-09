d9afb0c2876cee4506a440e2b21fe72fc65c24fa
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-08 19:11:51 -0500

    Delete demo items from taskList array

--- client/src/components/GoalOrganizer.tsx
+++ client/src/components/GoalOrganizer.tsx
@@ -4,46 +4,35 @@ import { GoalCards, useGoals } from "../context/GoalsContext";
 
 function GoalOrganizer() {
 
-    const { goalCards, setGoalCards } = useGoals();
-    const [idGoalCard, setIdGoalCard] = useState(1);
-   
-    function handleAddGoalCard() {
-      console.log(idGoalCard);
-      const newGoalCard: GoalCards = {
-        id: idGoalCard,
-        content: "First Goal 1",
-        isComplete: false,
-        taskList: [
-          {
-            id: 1,
-            content: "Mi lista 1",
-            isComplete: false
-          },
-          {
-            id: 2,
-            content: "Mi lista 2",
-            isComplete: true
-          }
-        ]
-      };
-  
-      setGoalCards([...goalCards, newGoalCard]);
-      console.log(newGoalCard);
-      setIdGoalCard(idGoalCard + 1);
-    }
-  
-    return (
-      <>
-        <div onClick={handleAddGoalCard}>
-          + Añadir Goal Card
-        </div>
-        <ul>
-          {goalCards.map(card => (
-            <GoalCard key={card.id} card={card} />
-          ))}
-        </ul>
-      </>
-    )
+  const { goalCards, setGoalCards } = useGoals();
+  const [idGoalCard, setIdGoalCard] = useState(1);
+
+  function handleAddGoalCard() {
+    console.log(idGoalCard);
+    const newGoalCard: GoalCards = {
+      id: idGoalCard,
+      content: "First Goal 1",
+      isComplete: false,
+      taskList: []
+    };
+
+    setGoalCards([...goalCards, newGoalCard]);
+    console.log(newGoalCard);
+    setIdGoalCard(idGoalCard + 1);
   }
-  
-  export default GoalOrganizer;
+
+  return (
+    <>
+      <div onClick={handleAddGoalCard}>
+        + Añadir Goal Card
+      </div>
+      <ul>
+        {goalCards.map(card => (
+          <GoalCard key={card.id} card={card} />
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default GoalOrganizer;
