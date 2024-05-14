af6f4d08e99103ba5f9461570c42448adbf40186
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-14 17:07:46 -0500

    Add header to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,9 @@ function App() {
 
   return (
     <div>
+      <header className="p-6 text-center bg-[#11353D] text-white">
+        <h1 className="text-2xl font-semibold">Organizador de Objetivos</h1>
+      </header>
       <GoalsContextProvider>
         <GoalFolder />
       </GoalsContextProvider>
