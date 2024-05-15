df3dfe539080c3443d0a7f714435b343530aa87d
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-15 03:16:44 -0500

    Add perspective and scrollbar styles for FolderViewer

--- client/src/components/FolderViewer.tsx
+++ client/src/components/FolderViewer.tsx
@@ -5,12 +5,12 @@ function FolderViewer() {
     const { goalOrganizer } = useGoals();
 
     return (
-        <div className="h-auto w-full text-center">
-            <ul className="flex h-auto justify-center relative pr-64">
-                {goalOrganizer.map((folder) => (
-                    <li key={folder.id} className="w-[300px] h-[319px] inline-flex relative -mr-[15em]">
-                        <span className="absolute bottom-0 z-30 text-vertical-custom -tracking-[0.25em] font-semibold text-slate-700 uppercase max-h-[9.50rem] mb-14 overflow-hidden transform-custom ml-9">{folder.name}</span>
-                        <img className="transform-custom h-full absolute" src={FolderView}></img>
+        <div className="h-[370px] w-full text-center">
+            <ul className="flex h-full pb-20 justify-start relative pr-64 overflow-x-scroll scrollbar-custom">
+                {goalOrganizer.map((folder, index) => (
+                    <li key={folder.id} className="min-w-[300px] w-[300px] h-[270px] inline-flex relative -mr-[15.5em] perspective-custom">
+                        <p className="absolute top-0 z-30 text-vertical-custom -tracking-[0.25em] font-semibold text-slate-700 uppercase max-h-[9.50rem] mt-20 overflow-hidden transform-custom ml-10">{folder.name}</p>
+                        <img className={`transform-custom h-full absolute ${index % 2 === 1 ? 'hue-rotate-15' : ''}`} src={FolderView}></img>
                     </li>
                 ))}
             </ul>
