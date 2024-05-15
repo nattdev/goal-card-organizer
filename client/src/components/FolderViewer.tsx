b65748defe6253bc4736f7199288e459d8bd7e21
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-15 17:08:02 -0500

    Fix z-index property of the p element on ios device

--- client/src/components/FolderViewer.tsx
+++ client/src/components/FolderViewer.tsx
@@ -8,9 +8,13 @@ function FolderViewer() {
         <div className="h-[370px] w-full text-center">
             <ul className="flex h-full pb-20 justify-start relative pr-64 overflow-x-scroll scrollbar-custom">
                 {goalOrganizer.map((folder, index) => (
-                    <li key={folder.id} className="min-w-[300px] w-[300px] h-[270px] inline-flex relative -mr-[15.5em] perspective-custom">
-                        <p className="absolute top-0 z-30 text-vertical-custom -tracking-[0.25em] font-semibold text-slate-700 uppercase max-h-[9.50rem] mt-20 overflow-hidden transform-custom ml-10">{folder.name}</p>
-                        <img className={`transform-custom h-full absolute ${index % 2 === 1 ? 'hue-rotate-15' : ''}`} src={FolderView}></img>
+                    <li key={folder.id} className="min-w-[300px] w-[300px] h-[270px] inline-flex relative -mr-[15.5em]">
+                        <div className="absolute top-0 z-30 perspective-custom">
+                            <p className="text-vertical-custom -tracking-[0.25em] font-semibold text-slate-700 uppercase max-h-[9.50rem] mt-20 overflow-hidden transform-custom ml-7">{folder.name}</p>
+                        </div>
+                        <div className="h-full absolute z-10 perspective-custom">
+                            <img className={`transform-custom h-full ${index % 2 === 1 ? 'hue-rotate-15' : ''}`} src={FolderView}></img>
+                        </div>
                     </li>
                 ))}
             </ul>
