200eff0f13d2e7e6b706e9aef5bb46b47324f2a0
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-16 17:20:31 -0500

    Update styles to fix compatibility for old ios device

--- client/src/components/FolderViewer.tsx
+++ client/src/components/FolderViewer.tsx
@@ -5,12 +5,12 @@ function FolderViewer() {
     const { goalOrganizer, handleOnChangeName } = useGoals();
 
     return (
-        <div className="h-[370px] w-full text-center">
-            <ul className="flex h-full pb-20 justify-start relative pr-64 overflow-x-scroll scrollbar-custom">
+        <div className="min-h-96 w-full text-center">
+            <ul className="flex h-full pb-20 justify-start relative pr-64 overflow-x-scroll scrollbar-custom gap-x-4 overflow-y-hidden pt-3">
                 {goalOrganizer.map((folder, index) => (
-                    <li key={folder.id} className="min-w-[300px] w-[300px] h-[270px] inline-flex relative -mr-[15.5em]">
-                        <div className="absolute top-0 z-30 perspective-custom">
-                            <input placeholder="Folder" className="text-vertical-custom -tracking-[0.25em] font-semibold text-slate-700 uppercase max-h-[9.50rem] mt-20 overflow-hidden transform-custom ml-7 bg-white bg-opacity-20" type="text" defaultValue={folder.name} onBlur={(e) => handleOnChangeName(e, folder.id)}></input>
+                    <li key={folder.id} className={`min-w-[300px] w-[300px] h-[270px] inline-flex relative -mr-[15.0em]`}>
+                        <div className="top-0 perspective-custom input-perspective z-30">
+                            <input placeholder="Folder" className="text-vertical-custom font-semibold text-slate-500 uppercase max-h-[9.50rem] mt-12 text-sm overflow-hidden transform-custom ml-7 bg-gray-200 bg-opacity-60 relative z-50" type="text" defaultValue={folder.name} onBlur={(e) => handleOnChangeName(e, folder.id)}></input>
                         </div>
                         <div className="h-full absolute z-10 perspective-custom">
                             <img className={`transform-custom h-full ${index % 2 === 1 ? 'hue-rotate-15' : ''}`} src={FolderView}></img>
