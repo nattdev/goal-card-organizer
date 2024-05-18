c1e24c473b9a984d9b57f1ac0c2755f3acbf3eba
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-18 00:30:29 -0500

    Fix input focus size

--- client/src/components/FolderViewer.tsx
+++ client/src/components/FolderViewer.tsx
@@ -6,14 +6,14 @@ function FolderViewer() {
 
     return (
         <div className="h-full w-full text-center">
-            <ul className="flex min-h-96 pb-20 justify-start relative pr-64 overflow-x-scroll scrollbar-custom gap-x-4 overflow-y-hidden pt-3" key={"1"}>
+            <ul className="min-h-96 flex justify-start scrollbar-custom pt-48 overflow-x-scroll overflow-y-hidden pl-28">
                 {goalOrganizer.map((folder, index) => (
-                    <li key={`${folder.id}-${index}`} className={`min-w-[300px] w-[300px] h-[270px] inline-flex relative -mr-[15.0em]`}>
-                        <div className="top-0 perspective-custom input-perspective z-30">
-                            <input placeholder="Folder" className="text-vertical-custom font-semibold text-slate-500 uppercase max-h-[9.50rem] mt-12 text-sm overflow-hidden transform-custom ml-7 bg-gray-200 bg-opacity-60 relative z-50" type="text" value={folder.name} onChange={(e) => handleOnChangeName(e, folder.id)}></input>
+                    <li key={`${folder.id}-${index}`} className={`transform-custom -m-14`}>
+                        <div className="">
+                            <input placeholder="Folder" className="relative z-50 text-slate-600 uppercase text-sm bg-gray-200 bg-opacity-20" type="text" value={folder.name} onChange={(e) => handleOnChangeName(e, folder.id)}></input>
                         </div>
-                        <div className="h-full absolute z-10 perspective-custom">
-                            <img className={`transform-custom h-full ${index % 2 === 1 ? 'hue-rotate-15' : ''}`} src={FolderView}></img>
+                        <div className="">
+                            <img className={`${index % 2 === 1 ? 'hue-rotate-15' : ''} absolute object-cover translate-y-[-43px] translate-x-[-10px]`} src={FolderView}></img>
                         </div>
                     </li>
                 ))}
