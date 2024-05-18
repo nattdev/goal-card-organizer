82da1c446af9d8e1020be8f79ad83ecaa2f2874f
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-18 17:59:21 -0500

    Update styles to FolderViewer

--- client/src/components/FolderViewer.tsx
+++ client/src/components/FolderViewer.tsx
@@ -5,15 +5,15 @@ function FolderViewer() {
     const { goalOrganizer, handleOnChangeName } = useGoals();
 
     return (
-        <div className="h-full w-full text-center">
-            <ul className="min-h-96 flex justify-start scrollbar-custom pt-48 overflow-x-scroll overflow-y-hidden pl-28">
+        <div className="relative h-96 text-center overflow-x-scroll overflow-y-hidden scrollbar-custom">
+            <ul className="flex h-full justify-start pt-40 pl-28">
                 {goalOrganizer.map((folder, index) => (
-                    <li key={`${folder.id}-${index}`} className={`transform-custom -m-14`}>
-                        <div className="">
-                            <input placeholder="Folder" className="relative z-50 text-slate-600 uppercase text-sm bg-gray-200 bg-opacity-20" type="text" value={folder.name} onChange={(e) => handleOnChangeName(e, folder.id)}></input>
+                    <li key={`${folder.id}-${index}`} className={`min-w-64 w-[250px] h-[198px] transform-custom perspective-custom -m-[90px]`}>
+                        <div className="transform-element-custom z-50 relative w-fit">
+                            <input placeholder="Folder" className="w-full relative z-50 text-slate-600 uppercase text-sm bg-gray-200 bg-opacity-60 font-medium" type="text" value={folder.name} onChange={(e) => handleOnChangeName(e, folder.id)}></input>
                         </div>
-                        <div className="">
-                            <img className={`${index % 2 === 1 ? 'hue-rotate-15' : ''} absolute object-cover translate-y-[-43px] translate-x-[-10px]`} src={FolderView}></img>
+                        <div className="w-full h-full transform-element-custom">
+                            <img className={`absolute object-cover translate-y-[-38px] translate-x-[-30px] w-full h-full ${index % 2 === 1 ? 'hue-rotate-15' : ''}`} src={FolderView}></img>
                         </div>
                     </li>
                 ))}
