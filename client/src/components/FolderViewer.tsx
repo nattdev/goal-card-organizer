1bdd571c2d2b89f6caa8d9dbebc350f0ec4f2b18
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-19 01:36:02 -0500

    Update translate property to the folder image

--- client/src/components/FolderViewer.tsx
+++ client/src/components/FolderViewer.tsx
@@ -14,7 +14,7 @@ function FolderViewer() {
                             <input placeholder="Folder" className="transform-element-custom w-full relative z-50 text-slate-600 uppercase text-sm bg-gray-200 bg-opacity-60 font-medium" type="text" value={folder.name} onChange={(e) => handleOnChangeName(e, folder.id)}></input>
                         </div>
                         <div className="w-full h-full transform-element-custom">
-                            <img className={`absolute object-cover translate-y-[-44px] translate-x-[-30px] w-full h-full ${index % 2 === 1 ? 'hue-rotate-15' : ''}`} src={FolderView}></img>
+                            <img className={`absolute object-cover sm:translate-y-[-37px] translate-y-[-34px] translate-x-[-30px] w-full sm:h-[239px] h-[204px] ${index % 2 === 1 ? 'hue-rotate-15' : ''}`} src={FolderView}></img>
                             <div className="absolute top-[-26px] right-12 cursor-pointer bg-slate-300 p-[2px] opacity-25 z-[500000]">
                                 <span className="" onClick={() => handleDeleteGoalFolder(folder.id)}>
                                     <img className="min-w-2 w-2" src={BoldCloseIcon}></img>
