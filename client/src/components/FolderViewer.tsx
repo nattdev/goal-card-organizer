a10a6b8b48fb223fe3c0e5643c6ec369d87f8aa3
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-21 02:02:14 -0500

    Fix display of elements in old iOS devices

--- client/src/components/FolderViewer.tsx
+++ client/src/components/FolderViewer.tsx
@@ -10,15 +10,17 @@ function FolderViewer() {
             <ul className="flex h-full justify-start pt-40 pl-28">
                 {goalOrganizer.map((folder, index) => (
                     <li key={`${folder.id}-${index}`} className={`min-w-64 sm:w-[300px] sm:h-[240px] sm:min-w-[300px] w-[250px] h-[223px] transform-custom perspective-custom -m-[90px]`}>
-                        <div className=" z-50 relative w-fit">
-                            <input placeholder="Folder" className="transform-element-custom w-full relative z-50 text-slate-600 uppercase text-sm bg-gray-200 bg-opacity-60 font-medium" type="text" value={folder.name} onChange={(e) => handleOnChangeName(e, folder.id)}></input>
-                        </div>
-                        <div className="w-full h-full transform-element-custom">
-                            <img className={`absolute object-cover sm:translate-y-[-37px] translate-y-[-34px] translate-x-[-30px] w-full sm:h-[239px] h-[204px] ${index % 2 === 1 ? 'hue-rotate-15' : ''}`} src={FolderView}></img>
-                            <div className="absolute top-[-26px] right-12 cursor-pointer bg-slate-300 p-[2px] opacity-25 z-[500000]" onClick={() => handleDeleteGoalFolder(folder.id)}>
-                                <span>
-                                    <img className="min-w-2 w-2" src={BoldCloseIcon}></img>
-                                </span>
+                        <div className="transform-element-custom">
+                            <div className="relative w-fit z-50">
+                                <input placeholder="Folder" className="w-full relative text-slate-600 uppercase text-sm bg-gray-200 bg-opacity-60 font-medium" type="text" value={folder.name} onChange={(e) => handleOnChangeName(e, folder.id)}></input>
+                            </div>
+                            <div className="w-full h-full">
+                                <img className={`absolute object-cover sm:translate-y-[-37px] translate-y-[-34px] translate-x-[-30px] w-full sm:h-[239px] h-[204px] ${index % 2 === 1 ? 'hue-rotate-15' : ''}`} src={FolderView}></img>
+                                <div className="absolute top-[-2px] right-12 cursor-pointer bg-slate-300 p-[2px] opacity-25" onClick={() => handleDeleteGoalFolder(folder.id)}>
+                                    <span>
+                                        <img className="min-w-2 w-2" src={BoldCloseIcon}></img>
+                                    </span>
+                                </div>
                             </div>
                         </div>
                     </li>
