7be28aeca771c69f5e50540d3b2ad05c80b8fd76
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-30 20:42:04 -0500

    Implement vite-plugin-pwa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  includeAssets: ['apple-touch-icon.png'],
+  workbox: {
+    globPatterns: ['**/*.{js,css,html,svg}'],
+  },
+  manifest: {
+    name: 'Goal Card Organizer',
+    short_name: 'Goal Card Organizer',
+    description: 'A web application to register goals',
+    theme_color: '#ffffff',
+    start_url: "/index.html",
+    icons: [
+      {
+        src: 'android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: 'apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png'
+      }
+    ]
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA(pwaOptions)
+  ]
 })
