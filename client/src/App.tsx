0a38bb5731eb27e0eebf7865f1c5964911b9b8bb
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-10-04 11:31:43 -0500

    Extract Header component into a separate file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,13 @@
 import FolderViewer from "./components/FolderViewer";
 import GoalFolder from "./components/GoalFolder";
+import Header from "./components/Header";
 import GoalsContextProvider from "./context/GoalsContext";
 
 function App() {
 
   return (
     <div>
-      <header className="p-6 text-center bg-[#11353D] text-white">
-        <h1 className="text-2xl font-semibold">Organizador de Objetivos</h1>
-      </header>
+      <Header/>
       <GoalsContextProvider>
         <FolderViewer />
         <GoalFolder />
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className="p-6 text-center bg-[#11353D] text-white">
+            <h1 className="text-2xl font-semibold">Organizador de Objetivos</h1>
+        </header>
+    )
+}
+
+export default Header;
